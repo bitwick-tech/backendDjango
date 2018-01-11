853b788e118b1ee9735538ceb746dae876990618
Neeraj <neerajlajpal@gmail.com>
2018-01-11 21:08:07 +0530

    mongo get data into api

--- coinDjango/views.py
+++ coinDjango/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
-
+import pymongo
+import json
+import pytz
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 from django.http import JsonResponse
 
 
+def get_data_from_mongo():
+    client = pymongo.MongoClient()
+    db = client.coinExchangeDB
+    collection = db.coinInfo
+    # res = collection.find().sort({_id: -1}).limit(1)
+    for d in collection.find():
+        res = d
+    res.pop('_id')
+    res["ts"] = res["ts"].replace(tzinfo=pytz.utc).timestamp()
+    return res
+
+
 def index(request):
-    return JsonResponse({'foo': 'bar'})
+    res = get_data_from_mongo()
+    return JsonResponse((res))
+    # ({'foo': 'bar'})
 
 # def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
