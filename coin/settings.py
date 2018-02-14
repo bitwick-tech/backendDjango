de8ac9749b9b900f30e46f205cd8e64e2397a97d
Ubuntu <ubuntu@ip-172-31-22-154.us-east-2.compute.internal>
2018-02-14 19:37:56 +0000

    server setup changes

--- coin/settings.py
+++ coin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'supctsf6*mc(plhiko$fzzj9fe(5c0$*-nejz$-2jd-dx@@-ke'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['smartcrypto.tech', '.smartcrypto.tech', '18.218.198.66']
 
 
 # Application definition
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'coin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'backenddjango',
+        'USER': 'ubuntu',
+        'PASSWORD': 'ubuntu',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -134,3 +145,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
--- coin/urls.py
+++ coin/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('coinDjango.urls')),
     path('admin/', admin.site.urls),
     path('coins/', include('coinDjango.urls')),
 ]
